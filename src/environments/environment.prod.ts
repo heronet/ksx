202439ae738b28edd3f0011d54c663a5310c13d0
heronet <sirat4757@gmail.com>
2021-09-23 23:51:50 +0600

    changed server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'https://mcq-t5in5dp7va-uc.a.run.app/api',
-  // BASE_URL: 'https://mcqbk.azurewebsites.net/api',
+  // BASE_URL: 'https://mcq-t5in5dp7va-uc.a.run.app/api',
+  BASE_URL: 'https://mcqbk.azurewebsites.net/api',
   // BASE_URL: 'https://mcqbk.herokuapp.com/api',
-
-  
 };
